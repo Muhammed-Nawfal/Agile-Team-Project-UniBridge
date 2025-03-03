5901962f0aae2d542673af0184df7a994b1ea33d
Muhammed Nawfal <you@example.com>
2025-03-03 20:15:24 +0000

    commit

--- src/main/webapp/app/entities/activity-match/list/activity-match.component.ts
+++ src/main/webapp/app/entities/activity-match/list/activity-match.component.ts
@@ -16,6 +16,7 @@ import { ActivityMatchDeleteDialogComponent } from '../delete/activity-match-del
   standalone: true,
   selector: 'jhi-activity-match',
   templateUrl: './activity-match.component.html',
+  styleUrl: 'activity-match.component.scss',
   imports: [
     RouterModule,
     FormsModule,
@@ -119,3 +120,24 @@ export class ActivityMatchComponent implements OnInit {
     });
   }
 }
+
+// @Component({
+//   standalone: true,
+//   selector: 'jhi-events-buddy',
+//   templateUrl: './events-buddy.component.html',
+//   styleUrl: 'events-buddy.component.scss',
+//   imports: [
+//     RouterModule,
+//     FormsModule,
+//     SharedModule,
+//   ],
+// })
+// export class EventsBuddyComponent implements OnInit {
+//   preferredSociety: string = '';
+//   preferredEventType: string = '';
+//   timing: string = '';
+//
+//   ngOnInit(): void {
+//     // Initialize component
+//   }
+// }
