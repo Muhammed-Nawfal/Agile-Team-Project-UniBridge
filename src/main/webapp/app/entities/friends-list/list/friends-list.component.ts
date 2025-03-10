2ac2b6e2e34f02e42bc20bdfca8744e6257752fd
hasaanafroze <hasaanafroze@gmail.com>
2025-03-10 15:15:17 +0000

    Linked friend list and friend request page

--- src/main/webapp/app/entities/friends-list/list/friends-list.component.ts
+++ src/main/webapp/app/entities/friends-list/list/friends-list.component.ts
@@ -11,6 +11,7 @@ import { DEFAULT_SORT_DATA, ITEM_DELETED_EVENT, SORT } from 'app/config/navigati
 import { IFriendsList } from '../friends-list.model';
 import { EntityArrayResponseType, FriendsListService } from '../service/friends-list.service';
 import { FriendsListDeleteDialogComponent } from '../delete/friends-list-delete-dialog.component';
+import { FriendRequestsComponent } from '../friend-requests/friend-requests.component'; // Import FriendRequestsComponent
 
 @Component({
   standalone: true,
@@ -26,6 +27,7 @@ import { FriendsListDeleteDialogComponent } from '../delete/friends-list-delete-
     DurationPipe,
     FormatMediumDatetimePipe,
     FormatMediumDatePipe,
+    FriendRequestsComponent,
   ],
 })
 export class FriendsListComponent implements OnInit {
