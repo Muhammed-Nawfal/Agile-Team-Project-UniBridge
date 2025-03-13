7b21919533c1ddf8555979dde175fb726d20c713
hasaanafroze <hasaanafroze@gmail.com>
2025-03-13 00:45:38 +0000

    friendsList is back

--- src/main/webapp/app/entities/friends-list/list/friends-list.component.ts
+++ src/main/webapp/app/entities/friends-list/list/friends-list.component.ts
@@ -11,11 +11,13 @@ import { DEFAULT_SORT_DATA, ITEM_DELETED_EVENT, SORT } from 'app/config/navigati
 import { IFriendsList } from '../friends-list.model';
 import { EntityArrayResponseType, FriendsListService } from '../service/friends-list.service';
 import { FriendsListDeleteDialogComponent } from '../delete/friends-list-delete-dialog.component';
+import { FriendRequestsComponent } from '../friend-requests/friend-requests.component';
 
 @Component({
   standalone: true,
   selector: 'jhi-friends-list',
   templateUrl: './friends-list.component.html',
+  styleUrls: ['./friends-list.component.scss'],
   imports: [
     RouterModule,
     FormsModule,
@@ -25,6 +27,7 @@ import { FriendsListDeleteDialogComponent } from '../delete/friends-list-delete-
     DurationPipe,
     FormatMediumDatetimePipe,
     FormatMediumDatePipe,
+    FriendRequestsComponent,
   ],
 })
 export class FriendsListComponent implements OnInit {
