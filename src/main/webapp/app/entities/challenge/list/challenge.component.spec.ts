34e579691f320f5d89ffc23ecc7c4adfaa3cd038
hasaanafroze <hasaanafroze@gmail.com>
2025-03-13 15:18:12 +0000

    final-frontend-m2

--- src/main/webapp/app/entities/challenge/list/challenge.component.spec.ts
+++ src/main/webapp/app/entities/challenge/list/challenge.component.spec.ts
@@ -84,7 +84,7 @@ describe('Challenge Management Component', () => {
 
     // THEN
     expect(service.query).toHaveBeenCalled();
-    expect(comp.challenges?.[0]).toEqual(expect.objectContaining({ id: 123 }));
+    expect(comp.challenges[0]).toEqual(expect.objectContaining({ id: 123 }));
   });
 
   describe('trackId', () => {
