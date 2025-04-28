1446bc044f687b54ffd56c984bfb815e09d6ce2a
Rohit <rohitmamtora04@gmail.com>
2025-04-28 18:27:08 +0100

    Re-adding old files from before the JDL regeneration. Additionally setup new azure VM

--- src/main/webapp/app/entities/profile/update/profile-update.component.ts
+++ src/main/webapp/app/entities/profile/update/profile-update.component.ts
@@ -1,104 +1,104 @@
-import { Component, ElementRef, OnInit, inject } from '@angular/core';
-import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { finalize, map } from 'rxjs/operators';
+import { FormBuilder } from '@angular/forms';
+import { HttpResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
-import SharedModule from 'app/shared/shared.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { AlertError } from 'app/shared/alert/alert-error.model';
-import { EventManager, EventWithContent } from 'app/core/util/event-manager.service';
-import { DataUtils, FileLoadError } from 'app/core/util/data-util.service';
-import { IUser } from 'app/entities/user/user.model';
-import { UserService } from 'app/entities/user/service/user.service';
-import { IMessageThread } from 'app/entities/message-thread/message-thread.model';
-import { MessageThreadService } from 'app/entities/message-thread/service/message-thread.service';
-import { Course } from 'app/entities/enumerations/course.model';
-import { University } from 'app/entities/enumerations/university.model';
-import { Skill } from 'app/entities/enumerations/skill.model';
-import { GymLocation } from 'app/entities/enumerations/gym-location.model';
-import { PreferredTime } from 'app/entities/enumerations/preferred-time.model';
-import { Sports } from 'app/entities/enumerations/sports.model';
-import { Society } from 'app/entities/enumerations/society.model';
-import { PreferredEvents } from 'app/entities/enumerations/preferred-events.model';
-import { ActivityType } from 'app/entities/enumerations/activity-type.model';
 import { ProfileService } from '../service/profile.service';
-import { IProfile } from '../profile.model';
-import { ProfileFormGroup, ProfileFormService } from './profile-form.service';
+import { IProfile, NewProfile } from '../profile.model';
+import { DataUtils } from 'app/core/util/data-util.service';
+import { ProfileFormService } from './profile-form.service';
 
 @Component({
-  standalone: true,
   selector: 'jhi-profile-update',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './profile-update.component.html',
-  imports: [SharedModule, FormsModule, ReactiveFormsModule],
+  styleUrls: ['./profile-update.component.scss'],
 })
 export class ProfileUpdateComponent implements OnInit {
   isSaving = false;
   profile: IProfile | null = null;
-  courseValues = Object.keys(Course);
-  universityValues = Object.keys(University);
-  skillValues = Object.keys(Skill);
-  gymLocationValues = Object.keys(GymLocation);
-  preferredTimeValues = Object.keys(PreferredTime);
-  sportsValues = Object.keys(Sports);
-  societyValues = Object.keys(Society);
-  preferredEventsValues = Object.keys(PreferredEvents);
-  activityTypeValues = Object.keys(ActivityType);
 
-  usersSharedCollection: IUser[] = [];
-  messageThreadsSharedCollection: IMessageThread[] = [];
+  // enumerations
+  courseValues = ['COMPUTER_SCIENCE', 'ECONOMICS', 'PSYCHOLOGY', 'MATHS', 'ENGLISH', 'ENGINEERING'];
+  skillValues = ['NOVICE', 'INTERMEDIATE', 'CONFIDENT', 'PROFESSIONAL'];
+  gymLocationValues = ['THE_GYM_SELLY_OAK', 'TIVERTON', 'PUREGYM_FIVE_WAYS'];
+  preferredTimeValues = ['EARLY', 'MORNING', 'AFTERNOON', 'EVENING', 'NIGHT', 'LATE'];
+  sportsValues = ['FOOTBALL', 'CRICKET', 'BADMINTON', 'TENNIS', 'NETBALL'];
 
-  protected dataUtils = inject(DataUtils);
-  protected eventManager = inject(EventManager);
-  protected profileService = inject(ProfileService);
-  protected profileFormService = inject(ProfileFormService);
-  protected userService = inject(UserService);
-  protected messageThreadService = inject(MessageThreadService);
-  protected elementRef = inject(ElementRef);
-  protected activatedRoute = inject(ActivatedRoute);
+  // Use the ProfileFormService to create the form
+  editForm = this.profileFormService.createProfileFormGroup();
+  showUpdateTip = true;
 
-  // eslint-disable-next-line @typescript-eslint/member-ordering
-  editForm: ProfileFormGroup = this.profileFormService.createProfileFormGroup();
-
-  compareUser = (o1: IUser | null, o2: IUser | null): boolean => this.userService.compareUser(o1, o2);
-
-  compareMessageThread = (o1: IMessageThread | null, o2: IMessageThread | null): boolean =>
-    this.messageThreadService.compareMessageThread(o1, o2);
+  constructor(
+    protected profileService: ProfileService,
+    protected profileFormService: ProfileFormService,
+    protected activatedRoute: ActivatedRoute,
+    protected fb: FormBuilder,
+    protected dataUtils: DataUtils,
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.data.subscribe(({ profile }) => {
-      this.profile = profile;
-      if (profile) {
-        this.updateForm(profile);
-      }
+    // Handle one-time tip for the current session
+    const tipDismissed = sessionStorage.getItem('updateProfileTipDismissed');
+    this.showUpdateTip = !tipDismissed;
 
-      this.loadRelationshipsOptions();
+    this.activatedRoute.data.subscribe(({ profile }) => {
+      if (profile?.id) {
+        this.profile = profile;
+        this.profileFormService.resetForm(this.editForm, profile);
+      } else {
+        this.previousState();
+      }
     });
+  }
+
+  dismissUpdateTip(): void {
+    this.showUpdateTip = false;
+    sessionStorage.setItem('updateProfileTipDismissed', 'true');
   }
 
   byteSize(base64String: string): string {
     return this.dataUtils.byteSize(base64String);
   }
 
-  openFile(base64String: string, contentType: string | null | undefined): void {
-    this.dataUtils.openFile(base64String, contentType);
-  }
-
   setFileData(event: Event, field: string, isImage: boolean): void {
-    this.dataUtils.loadFileToForm(event, this.editForm, field, isImage).subscribe({
-      error: (err: FileLoadError) =>
-        this.eventManager.broadcast(new EventWithContent<AlertError>('teamproject24App.error', { message: err.message })),
-    });
+    this.dataUtils.loadFileToForm(event, this.editForm, field, isImage).subscribe();
   }
 
+  // Fix for line 65
   clearInputImage(field: string, fieldContentType: string, idInput: string): void {
     this.editForm.patchValue({
       [field]: null,
       [fieldContentType]: null,
     });
-    if (idInput && this.elementRef.nativeElement.querySelector(`#${idInput}`)) {
-      this.elementRef.nativeElement.querySelector(`#${idInput}`).value = null;
+    const input = document.getElementById(idInput);
+    if (input instanceof HTMLInputElement) {
+      input.value = '';
+    }
+  }
+
+  save(): void {
+    this.isSaving = true;
+    const profileData = this.profileFormService.getProfile(this.editForm);
+
+    // Only proceed if we have a numeric ID (meaning it's an existing profile)
+    if (typeof profileData.id === 'number') {
+      const profile: IProfile = {
+        ...profileData,
+        id: profileData.id,
+      };
+
+      this.profileService.update(profile).subscribe({
+        next: () => this.onSaveSuccess(),
+        error: () => this.onSaveError(),
+      });
+    } else {
+      // Handle case where there's no valid ID
+      this.onSaveError();
+      console.error('Cannot save profile without a valid ID');
     }
   }
 
@@ -106,64 +106,12 @@ export class ProfileUpdateComponent implements OnInit {
     window.history.back();
   }
 
-  save(): void {
-    this.isSaving = true;
-    const profile = this.profileFormService.getProfile(this.editForm);
-    if (profile.id !== null) {
-      this.subscribeToSaveResponse(this.profileService.update(profile));
-    } else {
-      this.subscribeToSaveResponse(this.profileService.create(profile));
-    }
-  }
-
-  protected subscribeToSaveResponse(result: Observable<HttpResponse<IProfile>>): void {
-    result.pipe(finalize(() => this.onSaveFinalize())).subscribe({
-      next: () => this.onSaveSuccess(),
-      error: () => this.onSaveError(),
-    });
-  }
-
   protected onSaveSuccess(): void {
+    this.isSaving = false;
     this.previousState();
   }
 
   protected onSaveError(): void {
-    // Api for inheritance.
-  }
-
-  protected onSaveFinalize(): void {
     this.isSaving = false;
-  }
-
-  protected updateForm(profile: IProfile): void {
-    this.profile = profile;
-    this.profileFormService.resetForm(this.editForm, profile);
-
-    this.usersSharedCollection = this.userService.addUserToCollectionIfMissing<IUser>(this.usersSharedCollection, profile.user);
-    this.messageThreadsSharedCollection = this.messageThreadService.addMessageThreadToCollectionIfMissing<IMessageThread>(
-      this.messageThreadsSharedCollection,
-      ...(profile.messageThreads ?? []),
-    );
-  }
-
-  protected loadRelationshipsOptions(): void {
-    this.userService
-      .query()
-      .pipe(map((res: HttpResponse<IUser[]>) => res.body ?? []))
-      .pipe(map((users: IUser[]) => this.userService.addUserToCollectionIfMissing<IUser>(users, this.profile?.user)))
-      .subscribe((users: IUser[]) => (this.usersSharedCollection = users));
-
-    this.messageThreadService
-      .query()
-      .pipe(map((res: HttpResponse<IMessageThread[]>) => res.body ?? []))
-      .pipe(
-        map((messageThreads: IMessageThread[]) =>
-          this.messageThreadService.addMessageThreadToCollectionIfMissing<IMessageThread>(
-            messageThreads,
-            ...(this.profile?.messageThreads ?? []),
-          ),
-        ),
-      )
-      .subscribe((messageThreads: IMessageThread[]) => (this.messageThreadsSharedCollection = messageThreads));
   }
 }
