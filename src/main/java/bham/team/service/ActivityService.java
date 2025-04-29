8f3563db8d9a0b7f91fea923c173789494fff8f4
Aarij Khan <aarijomark@gmail.com>
2025-04-29 16:11:05 +0400

    created template for activity service

---
+++ src/main/java/bham/team/service/ActivityService.java
@@ -0,0 +1,65 @@
+package bham.team.service;
+
+import bham.team.domain.Activity;
+import bham.team.domain.enumeration.ActivityType;
+import bham.team.domain.enumeration.Status;
+import bham.team.repository.ActivityRepository;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ActivityService {
+
+    private final ActivityRepository activityRepository;
+
+    public ActivityService(ActivityRepository activityRepository) {
+        this.activityRepository = activityRepository;
+    }
+
+    /**
+     * Save an activity.
+     *
+     * @param activity the entity to save.
+     * @return the persisted entity.
+     */
+    public Activity save(Activity activity) {
+        return activityRepository.save(activity);
+    }
+
+    /**
+     * Get all activities.
+     *
+     * @param pageable the pagination information.
+     * @return the list of entities.
+     */
+    @Transactional(readOnly = true)
+    public Page<Activity> findAll(Pageable pageable) {
+        return activityRepository.findAll(pageable);
+    }
+
+    /**
+     * Get one activity by id.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    @Transactional(readOnly = true)
+    public Optional<Activity> findOne(Long id) {
+        return activityRepository.findById(id);
+    }
+
+    /**
+     * Delete the activity by id.
+     *
+     * @param id the id of the entity.
+     */
+    public void delete(Long id) {
+        activityRepository.deleteById(id);
+    }
+}
