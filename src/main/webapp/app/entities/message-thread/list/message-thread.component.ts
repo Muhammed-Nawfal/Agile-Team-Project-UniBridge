672ab7929bee732fa4d47d77ad184c7f7a5177ee
ubuntu <EXS415@student.bham.ac.uk>
2025-05-03 18:47:10 +0100

    Reduce conflicts

--- src/main/webapp/app/entities/message-thread/list/message-thread.component.ts
+++ src/main/webapp/app/entities/message-thread/list/message-thread.component.ts
@@ -1,6 +1,6 @@
 import { Component, NgZone, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Data, ParamMap, Router, RouterModule } from '@angular/router';
-import { Observable, Subscription, combineLatest, filter, tap } from 'rxjs';
+import { Observable, Subscription, combineLatest, filter, tap, take, switchMap } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import SharedModule from 'app/shared/shared.module';
@@ -11,6 +11,8 @@ import { DEFAULT_SORT_DATA, ITEM_DELETED_EVENT, SORT } from 'app/config/navigati
 import { IMessageThread } from '../message-thread.model';
 import { EntityArrayResponseType, MessageThreadService } from '../service/message-thread.service';
 import { MessageThreadDeleteDialogComponent } from '../delete/message-thread-delete-dialog.component';
+import { AccountService } from 'app/core/auth/account.service';
+import { ProfileService, EntityArrayResponseType as ProfileResponseType } from 'app/entities/profile/service/profile.service';
 
 @Component({
   standalone: true,
@@ -31,6 +33,7 @@ export class MessageThreadComponent implements OnInit {
   subscription: Subscription | null = null;
   messageThreads?: IMessageThread[];
   isLoading = false;
+  currentUserProfileId?: number;
 
   sortState = sortStateSignal({});
 
@@ -40,26 +43,44 @@ export class MessageThreadComponent implements OnInit {
   protected readonly sortService = inject(SortService);
   protected modalService = inject(NgbModal);
   protected ngZone = inject(NgZone);
+  protected accountService = inject(AccountService);
+  protected profileService = inject(ProfileService);
 
-  trackId = (item: IMessageThread): number => this.messageThreadService.getMessageThreadIdentifier(item);
+  trackId = (_index: number, item: IMessageThread): number => this.messageThreadService.getMessageThreadIdentifier(item);
 
   ngOnInit(): void {
-    this.subscription = combineLatest([this.activatedRoute.queryParamMap, this.activatedRoute.data])
+    // 1) First resolve my profile ID
+    this.accountService
+      .identity()
       .pipe(
-        tap(([params, data]) => this.fillComponentAttributeFromRoute(params, data)),
-        tap(() => {
-          if (!this.messageThreads || this.messageThreads.length === 0) {
-            this.load();
-          }
-        }),
+        take(1),
+        switchMap(account => this.profileService.query({ 'userLogin.equals': account?.login }).pipe(take(1))),
       )
-      .subscribe();
+      .subscribe(resp => {
+        const me = resp.body?.[0];
+        if (me?.id) {
+          this.currentUserProfileId = me.id;
+
+          // 2) Now that we have my profile ID, continue with initialization
+          this.subscription = combineLatest([this.activatedRoute.queryParamMap, this.activatedRoute.data])
+            .pipe(
+              tap(([params, data]) => this.fillComponentAttributeFromRoute(params, data)),
+              tap(() => {
+                if (!this.messageThreads || this.messageThreads.length === 0) {
+                  this.load();
+                }
+              }),
+            )
+            .subscribe();
+        } else {
+          console.error('Could not find my profile');
+        }
+      });
   }
 
   delete(messageThread: IMessageThread): void {
     const modalRef = this.modalService.open(MessageThreadDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
     modalRef.componentInstance.messageThread = messageThread;
-    // unsubscribe not needed because closed completes on modal close
     modalRef.closed
       .pipe(
         filter(reason => reason === ITEM_DELETED_EVENT),
@@ -78,6 +99,29 @@ export class MessageThreadComponent implements OnInit {
 
   navigateToWithComponentValues(event: SortState): void {
     this.handleNavigation(event);
+  }
+
+  openChat(messageThread: IMessageThread): void {
+    this.router.navigate(['/chat'], {
+      queryParams: { threadId: messageThread.id },
+    });
+  }
+
+  getOtherParticipant(thread: IMessageThread): string {
+    if (thread.isGroup) {
+      return thread.name ?? 'Group Chat';
+    }
+
+    // For friend chats, find the other participant using profile ID
+    if (thread.friendChat) {
+      if (thread.friendChat.requestedByProfile?.id === this.currentUserProfileId) {
+        return `${thread.friendChat.requestedToProfile?.firstName} ${thread.friendChat.requestedToProfile?.lastName}`;
+      } else {
+        return `${thread.friendChat.requestedByProfile?.firstName} ${thread.friendChat.requestedByProfile?.lastName}`;
+      }
+    }
+
+    return 'Unknown User';
   }
 
   protected fillComponentAttributeFromRoute(params: ParamMap, data: Data): void {
